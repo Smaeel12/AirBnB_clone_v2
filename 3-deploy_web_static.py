4b6e256a781d0f2ab136f6e3c9a71740964e41ef
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-07 21:25:25 +0100

    header

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,18 +1,16 @@
-##!/usr/bin/python3
-""" Fabric script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers, using the function do_deploy
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack
 """
 from datetime import datetime
-from fabric.api import put, run, local, env
+from fabric.api import local
 from os import path
-
-env.hosts = ['54.157.150.179', '34.207.237.246']
 
 
 def do_pack():
-    """ do_pack function
-    Return: the archive path if the archive has been correctly generated.
-    Otherwise, it should return None
+    """ pack function
+    Return: the archive path if the archive has been correctly generated,
+    otherwise, it should return None
     """
     try:
         if not path.exists('versions'):
