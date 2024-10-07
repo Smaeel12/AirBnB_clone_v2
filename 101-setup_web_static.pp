5ede0c2213306221338d2fde4a942e0575c212e0
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-07 22:59:01 +0100

    more

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,28 +1,29 @@
 # HTML content for index.html
 $indexfile = @(END)
-<html >
-<head >
-< / head >
-<body >
-Holberton School
-< / body >
-< / html >
+<html>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
+</html>
 END
 
 # Nginx configuration content
 $nginxconfig = @(END)
 server {
-		listen 80
-		listen[::]: 80
+    listen 80;
+    listen [::]:80;
 
-		root / data / web_static / current /
-		server_name _
-		index index.html
+    root /data/web_static/current/;
+    server_name _;
 
-		location / hbnb_static {
-				alias / data / web_static / current /
-				autoindex off
-		}
+    index index.html;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        autoindex off;
+    }
 }
 END
 
@@ -31,61 +32,61 @@ $folders = ['/data/', '/data/web_static/', '/data/web_static/releases/',
 
 # Update and upgrade system packages
 exec {'apt update':
-  command = > '/usr/bin/apt update -y',
+  command => '/usr/bin/apt update -y',
 }
 
 exec {'apt upgrade':
-  command = > '/usr/bin/apt upgrade -y',
-  require = > Exec['apt update'],
+  command => '/usr/bin/apt upgrade -y',
+  require => Exec['apt update'],
 }
 
 # Ensure Nginx is installed
 package {'nginx':
-  ensure = > 'installed',
-  require = > Exec['apt upgrade'],
-  before = > Service['nginx']
+  ensure => 'installed',
+  require => Exec['apt upgrade'],
+  before => Service['nginx']
 }
 
 # Ensure Nginx service is running and enabled
 service {'nginx':
-  ensure = > 'running',
-  enable = > true,
-  require = > Package['nginx'],
-  subscribe = > File['/etc/nginx/sites-enabled/default']
+  ensure => 'running',
+  enable => true,
+  require => Package['nginx'],
+  subscribe => File['/etc/nginx/sites-enabled/default']
 }
 
 # Create the necessary directory structure using the file resource
 file {$folders:
-  ensure = > 'directory',
-  owner = > 'ubuntu',
-  group = > 'ubuntu',
+  ensure => 'directory',
+  owner => 'ubuntu',
+  group => 'ubuntu',
 }
 
 # Create a symbolic link to the current release
 file {'/data/web_static/current':
-  ensure = > 'link',
-  target = > '/data/web_static/releases/test/',
-  require = > File['/data/web_static/releases/test/']
+  ensure => 'link',
+  target => '/data/web_static/releases/test/',
+  require => File['/data/web_static/releases/test/']
 }
 
 # Create the index.html file with the specified content
 file {'/data/web_static/releases/test/index.html':
-  ensure = > 'present',
-  content = > $indexfile,
-  owner = > 'ubuntu',
-  group = > 'ubuntu',
-  require = > File['/data/web_static/releases/test/'],
+  ensure => 'present',
+  content => $indexfile,
+  owner => 'ubuntu',
+  group => 'ubuntu',
+  require => File['/data/web_static/releases/test/'],
 }
 
 # Place the Nginx config file in the correct location
 file {'/etc/nginx/sites-available/airbnb':
-  ensure = > 'present',
-  content = > $nginxconfig,
-  require = > Package['nginx'],
+  ensure => 'present',
+  content => $nginxconfig,
+  require => Package['nginx'],
 }
 
 file {'/etc/nginx/sites-enabled/default':
-  ensure = > 'link',
-  target = > '/etc/nginx/sites-available/airbnb',
-  require = > File['/etc/nginx/sites-available/airbnb'],
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/airbnb',
+  require => File['/etc/nginx/sites-available/airbnb'],
 }
